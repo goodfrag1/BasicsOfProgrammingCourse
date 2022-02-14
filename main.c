6c1e6dc596d0b2f39b0ff4d5c6494be378757e4e
goodfrag1 <GiGaBiT2017@yandex.ru>
2022-02-14 18:42:44 +0300

    7 task

--- main.c
+++ main.c
@@ -2,17 +2,36 @@
 #include "Libs/data_structures/matrix/matrix.h"
 #include "Libs/algorithms/functions/function.h"
 
-int getMin(int *a, int n) {
-    int minElement = a[0];
-    for (size_t i = 1; i < n; i++)
-        if (a[i] < minElement)
-            minElement = a[i];
-
-    return minElement;
+int max(int a, int b) {
+    return a > b ? a : b;
 }
 
-void sortColsByMinElement(matrix m) {
-        insertionSortColsMatrixByColCriteria(m, getMin);
+long long findSumOfMaxesOfPseudoDiagonal(matrix m) {
+    long long sum = 0;
+    for (int i = 1; i < m.nCols; ++i) {
+        int j = i;
+        int k = 0;
+        int maxElement = m.values[k][j];
+        while (j < m.nCols) {
+            maxElement = max(maxElement, m.values[k][j]);
+            j++;
+            k++;
+        }
+        sum += maxElement;
+    }
+    for (int i = 1; i < m.nRows; ++i) {
+        int j = i;
+        int k = 0;
+        int maxElement = m.values[j][k];
+        while (j < m.nRows) {
+            maxElement = max(maxElement, m.values[j][k]);
+            j++;
+            k++;
+        }
+        sum += maxElement;
+    }
+
+    return sum;
 }
 
 int main() {
@@ -24,9 +43,7 @@ int main() {
     matrix m = getMemMatrix(nRows, nCols);
     inputMatrix(m);
 
-    sortColsByMinElement(m);
-
-    outputMatrix(m);
+    printf("%lld", findSumOfMaxesOfPseudoDiagonal(m));
 
     return 0;
 }

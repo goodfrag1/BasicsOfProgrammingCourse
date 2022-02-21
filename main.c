1edc40cde363fe38d969810773fab2b6f5e9462a
goodfrag1 <GiGaBiT2017@yandex.ru>
2022-02-21 21:21:01 +0300

    13 task

--- main.c
+++ main.c
@@ -2,27 +2,44 @@
 #include "Libs/data_structures/matrix/matrix.h"
 #include "Libs/algorithms/functions/function.h"
 
-position getLeftMin(matrix m) {
-    return getMinValuePos(m);
+bool isNonDescendingSorted(const int *a, int n) {
+    for (size_t i = 0; i < n - 1; i++)
+        if (a[i] > a[i + 1])
+            return false;
+
+    return true;
 }
 
-void swapPenultimateRow(matrix m) {
-    position minElementPos = getLeftMin(m);
-    int minElementCol = minElementPos.colIndex;
-    for (int i = m.nRows - 1; i >= 0; --i)
-        m.values[m.nRows - 2][i] = m.values[i][minElementCol];
+bool hasAllNonDescendingRows(matrix m) {
+    for (int i = 0; i < m.nRows; ++i)
+        if (!isNonDescendingSorted(m.values[i], m.nCols))
+            return false;
+
+    return true;
 }
+
+int countNonDescendingRowsMatrices(matrix *ms, int nMatrix) {
+    int count = 0;
+    for (int i = 0; i < nMatrix; ++i) {
+        count += hasAllNonDescendingRows(ms[i]);
+    }
+
+    return count;
+}
+
 
 int main() {
+    int nMatrices;
+    scanf("%d", &nMatrices);
     int nRows;
     scanf("%d", &nRows);
+    int nCols;
+    scanf("%d", &nCols);
 
-    matrix m = getMemMatrix(nRows, nRows);
-    inputMatrix(m);
+    matrix *ms = getMemArrayOfMatrices(nMatrices, nRows, nCols);
+    inputMatrices(ms, nMatrices);
 
-    swapPenultimateRow(m);
-
-    outputMatrix(m);
+    printf("%d", countNonDescendingRowsMatrices(ms, nMatrices));
 
     return 0;
 }

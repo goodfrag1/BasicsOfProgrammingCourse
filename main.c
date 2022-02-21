315b0420decbdc009029d1f0d4b79f2c70da7e4b
goodfrag1 <GiGaBiT2017@yandex.ru>
2022-02-21 21:10:31 +0300

    12 task

--- main.c
+++ main.c
@@ -2,36 +2,27 @@
 #include "Libs/data_structures/matrix/matrix.h"
 #include "Libs/algorithms/functions/function.h"
 
-bool checkIsSpecialElement(matrix m, size_t k) {
-    int max = m.values[0][k];
-    int sum = 0;
-    for (int i = 0; i < m.nRows; ++i) {
-        sum += m.values[i][k];
-        if (m.values[i][k] > max)
-            max = m.values[i][k];
-    }
-
-    return sum - max < max;
+position getLeftMin(matrix m) {
+    return getMinValuePos(m);
 }
 
-int getNSpecialElement(matrix m) {
-    int count = 0;
-    for (size_t i = 0; i < m.nCols; ++i)
-        count += checkIsSpecialElement(m, i);
-
-    return count;
+void swapPenultimateRow(matrix m) {
+    position minElementPos = getLeftMin(m);
+    int minElementCol = minElementPos.colIndex;
+    for (int i = m.nRows - 1; i >= 0; --i)
+        m.values[m.nRows - 2][i] = m.values[i][minElementCol];
 }
 
 int main() {
     int nRows;
     scanf("%d", &nRows);
-    int nCols;
-    scanf("%d", &nCols);
 
-    matrix m = getMemMatrix(nRows, nCols);
+    matrix m = getMemMatrix(nRows, nRows);
     inputMatrix(m);
 
-    printf("%d", getNSpecialElement(m));
+    swapPenultimateRow(m);
+
+    outputMatrix(m);
 
     return 0;
 }

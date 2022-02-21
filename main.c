a8f7de2e99be126f14a38765aa1b5abfa62e8090
goodfrag1 <GiGaBiT2017@yandex.ru>
2022-02-21 20:58:15 +0300

    11 task

--- main.c
+++ main.c
@@ -2,37 +2,24 @@
 #include "Libs/data_structures/matrix/matrix.h"
 #include "Libs/algorithms/functions/function.h"
 
-long long getSum(const int *a, int n) {
-    long long sum = 0;
-    for (int i = 0; i < n; ++i)
-        sum += a[i];
-
-    return sum;
-}
-
-int cmp_long_long(const void *pa, const void *pb) {
-    return memcmp(pa, pb, sizeof(long long));
-}
-
-int countNUnique(long long *a, int n) {
-    int count = 0;
-    for (int i = 0; i < n; ++i) {
-        int j = 0;
-        while (j < i && cmp_long_long(a + j, a + i))
-            j++;
-        count += j == i;
+bool checkIsSpecialElement(matrix m, size_t k) {
+    int max = m.values[0][k];
+    int sum = 0;
+    for (int i = 0; i < m.nRows; ++i) {
+        sum += m.values[i][k];
+        if (m.values[i][k] > max)
+            max = m.values[i][k];
     }
 
-    return count;
+    return sum - max < max;
 }
 
-int countEqClassesByRowsSum(matrix m) {
-    long long arrayOfSums[m.nRows];
-    for (int i = 0; i < m.nRows; ++i)
-        arrayOfSums[i] = getSum(m.values[i], m.nCols);
-    int countOfUnique = countNUnique(arrayOfSums, m.nRows);
+int getNSpecialElement(matrix m) {
+    int count = 0;
+    for (size_t i = 0; i < m.nCols; ++i)
+        count += checkIsSpecialElement(m, i);
 
-    return countOfUnique;
+    return count;
 }
 
 int main() {
@@ -44,7 +31,7 @@ int main() {
     matrix m = getMemMatrix(nRows, nCols);
     inputMatrix(m);
 
-    printf("%d", countEqClassesByRowsSum(m));
+    printf("%d", getNSpecialElement(m));
 
     return 0;
 }

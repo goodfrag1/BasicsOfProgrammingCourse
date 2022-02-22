38c6277f948119e7388d116c64c9f79197b52979
goodfrag1 <GiGaBiT2017@yandex.ru>
2022-02-22 21:03:45 +0300

    5 task(tests)

--- main.c
+++ main.c
@@ -2,42 +2,74 @@
 #include "Libs/data_structures/matrix/matrix.h"
 #include "Libs/algorithms/functions/function.h"
 
-matrix mulMatrices(matrix m1, matrix m2) {
-    matrix resM = getMemMatrix(m1.nRows, m2.nCols);
-    for (int i = 0; i < m1.nRows; ++i)
-        for (int j = 0; j < m2.nCols; ++j) {
-            resM.values[i][j] = 0;
-            for (int k = 0; k < m2.nRows; ++k)
-                resM.values[i][j] += m1.values[i][k] * m2.values[k][j];
-        }
-    return resM;
+long long getSum(const int *a, int n) {
+    long long sum = 0;
+    for (int i = 0; i < n; ++i)
+        sum += a[i];
+
+    return sum;
 }
 
-bool isMutuallyInverseMatrices(matrix m1, matrix m2) {
-    return isEMatrix(mulMatrices(m1, m2));
+bool isUnique(const long long *a, int n) {
+    for (int i = 0; i < n - 1; ++i)
+        for (int j = i + 1; j < n - 1; ++j)
+            if (a[i] == a[j])
+                return false;
+
+    return true;
 }
 
-void test_isMutuallyInverseMatrices() {
-    matrix m1 = createMatrixFromArray((int[]) {1, 2, 3, 4}, 2, 2);
-    matrix m2 = createMatrixFromArray((int[]) {1, 2, 3, 4}, 2, 2);
+void transposeIfMatrixHasEqualSumOfRows(matrix m, int nRows, int nCols) {
+    long long arrayOfSums[m.nRows];
+    for (int i = 0; i < m.nRows; ++i)
+        arrayOfSums[i] = getSum(m.values[i], m.nRows);
 
-    transposeSquareMatrix(m2);
+    if (!isUnique(arrayOfSums, nRows))
+        transposeSquareMatrix(m);
+}
+
+void test_getSum() {
+    int a[] = {1, 2, 3, 4};
+
+    assert(getSum(a, 4) == 10);
+}
+
+void test_isUnique_true() {
+    long long a[] = {1, 2, 3, 4};
+
+    assert(isUnique(a, 4));
+}
+
+void test_isUnique_false() {
+    long long a[] = {1, 1, 3, 4};
+
+    assert(!isUnique(a, 4));
+}
+
+void test_transposeIfMatrixHasEqualSumOfRows_equalSum() {
+    matrix m1 = createMatrixFromArray((int[]) {1, 6, 3, 4}, 2, 2);
+    transposeIfMatrixHasEqualSumOfRows(m1, 2, 2);
+
+    matrix m2 = createMatrixFromArray((int[]) {3, 1, 6, 4}, 2, 2);
 
     assert(twoMatricesEqual(m1, m2));
 }
 
-void test_mulMatrices() {
-    matrix m1 = createMatrixFromArray((int[]) {1, 2, 3, 4}, 2, 2);
-    matrix m2 = createMatrixFromArray((int[]) {4, 3, 2, 1}, 2, 2);
+void test_transposeIfMatrixHasEqualSumOfRows_notEqualSum() {
+    matrix m1 = createMatrixFromArray((int[]) {1, 5, 3, 4}, 2, 2);
+    transposeIfMatrixHasEqualSumOfRows(m1, 2, 2);
 
-    matrix resM = createMatrixFromArray((int[]) {8, 5, 20, 13}, 2, 2);
+    matrix m2 = createMatrixFromArray((int[]) {1, 5, 3, 4}, 2, 2);
 
-    assert(twoMatricesEqual(resM, mulMatrices(m1, m2)));
+    assert(twoMatricesEqual(m1, m2));
 }
 
 void test() {
-    test_mulMatrices();
-    test_isMutuallyInverseMatrices();
+    test_getSum();
+    test_isUnique_true();
+    test_isUnique_false();
+    test_transposeIfMatrixHasEqualSumOfRows_equalSum();
+    test_transposeIfMatrixHasEqualSumOfRows_notEqualSum();
 }
 
 int main() {
@@ -46,13 +78,12 @@ int main() {
     int nRows;
     scanf("%d", &nRows);
 
-    matrix A = getMemMatrix(nRows, nRows);
-    inputMatrix(A);
-    matrix B = getMemMatrix(nRows, nRows);
-    inputMatrix(B);
+    matrix m = getMemMatrix(nRows, nRows);
+    inputMatrix(m);
 
-    printf("%d", isMutuallyInverseMatrices(A, B));
+    transposeIfMatrixHasEqualSumOfRows(m, m.nRows, m.nCols);
+
+    outputMatrix(m);
 
     return 0;
 }
-

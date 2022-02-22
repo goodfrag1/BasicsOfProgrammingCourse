ee2dfa5290f214c7eac73a108b08f765ecc39e4d
goodfrag1 <GiGaBiT2017@yandex.ru>
2022-02-22 20:48:04 +0300

    6 task(tests)

--- main.c
+++ main.c
@@ -2,47 +2,42 @@
 #include "Libs/data_structures/matrix/matrix.h"
 #include "Libs/algorithms/functions/function.h"
 
-int max(int a, int b) {
-    return a > b ? a : b;
+matrix mulMatrices(matrix m1, matrix m2) {
+    matrix resM = getMemMatrix(m1.nRows, m2.nCols);
+    for (int i = 0; i < m1.nRows; ++i)
+        for (int j = 0; j < m2.nCols; ++j) {
+            resM.values[i][j] = 0;
+            for (int k = 0; k < m2.nRows; ++k)
+                resM.values[i][j] += m1.values[i][k] * m2.values[k][j];
+        }
+    return resM;
 }
 
-long long findSumOfMaxesOfPseudoDiagonal(matrix m) {
-    int n = m.nRows + m.nCols - 1;
-    int *maxElements = (int *) (calloc(n, sizeof(int)));
-
-    int index = m.nRows - 1;
-    long long sum = 0;
-    for (int i = 0; i < m.nRows; ++i)
-        for (int j = 0; j < m.nCols; ++j)
-            if (i != j) {
-                int k = index - i + j;
-                if (i == 0 || j == 0)
-                    maxElements[k] = m.values[i][j];
-                else
-                    maxElements[k] = max(maxElements[k], m.values[i][j]);
-            }
-
-    for (int i = 0; i < n; ++i)
-        sum += maxElements[i];
-
-    return sum;
+bool isMutuallyInverseMatrices(matrix m1, matrix m2) {
+    return isEMatrix(mulMatrices(m1, m2));
 }
 
-void test_findSumOfMaxesOfPseudoDiagonal_notOneElement() {
-    matrix m = createMatrixFromArray((int[]) {3, 2, 5, 4, 1, 3, 6, 3, 3, 2, 1, 2}, 3, 4);
+void test_isMutuallyInverseMatrices() {
+    matrix m1 = createMatrixFromArray((int[]) {1, 2, 3, 4}, 2, 2);
+    matrix m2 = createMatrixFromArray((int[]) {1, 2, 3, 4}, 2, 2);
 
-    assert(findSumOfMaxesOfPseudoDiagonal(m) == 20);
+    transposeSquareMatrix(m2);
+
+    assert(twoMatricesEqual(m1, m2));
 }
 
-void test_findSumOfMaxesOfPseudoDiagonal_oneElement() {
-    matrix m = createMatrixFromArray((int[]) {3}, 1, 1);
+void test_mulMatrices() {
+    matrix m1 = createMatrixFromArray((int[]) {1, 2, 3, 4}, 2, 2);
+    matrix m2 = createMatrixFromArray((int[]) {4, 3, 2, 1}, 2, 2);
 
-    assert(findSumOfMaxesOfPseudoDiagonal(m) == 0);
+    matrix resM = createMatrixFromArray((int[]) {8, 5, 20, 13}, 2, 2);
+
+    assert(twoMatricesEqual(resM, mulMatrices(m1, m2)));
 }
 
 void test() {
-    test_findSumOfMaxesOfPseudoDiagonal_notOneElement();
-    test_findSumOfMaxesOfPseudoDiagonal_oneElement();
+    test_mulMatrices();
+    test_isMutuallyInverseMatrices();
 }
 
 int main() {
@@ -50,13 +45,13 @@ int main() {
 
     int nRows;
     scanf("%d", &nRows);
-    int nCols;
-    scanf("%d", &nCols);
 
-    matrix m = getMemMatrix(nRows, nCols);
-    inputMatrix(m);
+    matrix A = getMemMatrix(nRows, nRows);
+    inputMatrix(A);
+    matrix B = getMemMatrix(nRows, nRows);
+    inputMatrix(B);
 
-    printf("%lld", findSumOfMaxesOfPseudoDiagonal(m));
+    printf("%d", isMutuallyInverseMatrices(A, B));
 
     return 0;
 }

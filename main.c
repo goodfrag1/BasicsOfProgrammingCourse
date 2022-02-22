e8e64ab0f8ac48e9205fe738e20ab5d2dcc904f7
goodfrag1 <GiGaBiT2017@yandex.ru>
2022-02-22 20:17:57 +0300

    8 task(tests)

--- main.c
+++ main.c
@@ -2,51 +2,39 @@
 #include "Libs/data_structures/matrix/matrix.h"
 #include "Libs/algorithms/functions/function.h"
 
-#define EPS 1e-8
-
-float getDistance(int *a, int n) {
-    long long sum = 0;
-    for (int i = 0; i < n; ++i)
-        sum += pow(a[i], 2);
-
-    return sqrtf(sum);
-}
-
-void insertionSortRowsMatrixByRowCriteriaF(matrix m, float (*criteria)(int *, int)) {
-    float *resultsOfCriteria = (float *) malloc(m.nRows * sizeof(float));
-    for (int i = 0; i < m.nRows; ++i)
-        resultsOfCriteria[i] = criteria(m.values[i], m.nCols);
-    for (int i = 0; i < m.nRows; ++i) {
-        int index = i;
-        while (resultsOfCriteria[index] < resultsOfCriteria[index - 1] && index > 0) {
-            swapF(&resultsOfCriteria[index], &resultsOfCriteria[index - 1]);
-            swapRows(m, index, index - 1);
-            index--;
-        }
+int getMinInArea(matrix m) {
+    position maxPos = getMaxValuePos(m);
+    int h = maxPos.rowIndex - 1;
+    int leftBorder = maxPos.colIndex - 1;
+    int rightBorder = maxPos.colIndex + 1;
+    int minElement = m.values[maxPos.rowIndex][maxPos.colIndex];
+    while (h >= 0) {
+        for (int i = leftBorder; i < rightBorder; i++)
+            if (m.values[h][i] < minElement)
+                minElement = m.values[h][i];
+        h--;
+        leftBorder--;
+        rightBorder++;
     }
-    free(resultsOfCriteria);
+
+    return minElement;
 }
 
-void sortByDistances(matrix m) {
-    insertionSortRowsMatrixByRowCriteriaF(m, getDistance);
+void test_getMinInArea_notEdgeMin() {
+    matrix m = createMatrixFromArray((int[]) {10, 7, 5, 6, 3, 11, 8, 9, 4, 1, 12, 2}, 3, 4);
+
+    assert(getMinInArea(m) == 5);
 }
 
-void test_getDistance() {
-    int a[] = {1, 2, 3, 4};
+void test_getMinInArea_edgeMin() {
+    matrix m = createMatrixFromArray((int[]) {6, 8, 9, 2, 7, 12, 3, 4, 10, 11, 5, 1}, 3, 4);
 
-    assert((getDistance(a, 4) - sqrt(30)) > EPS);
-}
-
-void test_sortByDistance() {
-    matrix m1 = createMatrixFromArray((int[]) {4, 3, 2, 1}, 2, 2);
-    matrix m2 = createMatrixFromArray((int[]) {1, 2, 3, 4}, 2, 2);
-
-    assert(twoMatricesEqual(m1, m2));
+    assert(getMinInArea(m) == 6);
 }
 
 void test() {
-    test_getDistance();
-    test_sortByDistance();
+    test_getMinInArea_edgeMin();
+    test_getMinInArea_notEdgeMin();
 }
 
 int main() {
@@ -60,9 +48,8 @@ int main() {
     matrix m = getMemMatrix(nRows, nCols);
     inputMatrix(m);
 
-    sortByDistances(m);
-
-    outputMatrix(m);
+    printf("%d", getMinInArea(m));
 
     return 0;
 }
+

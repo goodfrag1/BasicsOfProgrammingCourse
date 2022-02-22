6981ecb88edfabaa39596137283df04d6694d19e
goodfrag1 <GiGaBiT2017@yandex.ru>
2022-02-22 15:51:36 +0300

    12 task(tests)

--- main.c
+++ main.c
@@ -2,97 +2,48 @@
 #include "Libs/data_structures/matrix/matrix.h"
 #include "Libs/algorithms/functions/function.h"
 
-bool isNonDescendingSorted(const int *a, int n) {
-    for (size_t i = 0; i < n - 1; i++)
-        if (a[i] >= a[i + 1])
-            return false;
-
-    return true;
+position getLeftMin(matrix m) {
+    return getMinValuePos(m);
 }
 
-bool hasAllNonDescendingRows(matrix m) {
-    for (int i = 0; i < m.nRows; ++i)
-        if (!isNonDescendingSorted(m.values[i], m.nCols))
-            return false;
-
-    return true;
+void swapPenultimateRow(matrix m) {
+    position minElementPos = getLeftMin(m);
+    int minElementCol = minElementPos.colIndex;
+    for (int i = m.nRows - 1; i >= 0; --i)
+        m.values[m.nRows - 2][i] = m.values[i][minElementCol];
 }
 
-int countNonDescendingRowsMatrices(matrix *ms, int nMatrix) {
-    int count = 0;
-    for (int i = 0; i < nMatrix; ++i) {
-        count += hasAllNonDescendingRows(ms[i]);
-    }
+void test_swapPenultimateRow_oneElement() {
+    matrix m1 = createMatrixFromArray((int[]) {1}, 1, 1);
+    matrix m2 = createMatrixFromArray((int[]) {1}, 1, 1);
 
-    return count;
+    assert(twoMatricesEqual(m1, m2));
 }
 
-void test_isNonDescendingSorted_false() {
-    int a[] = {1, 2, 3, 4};
+void test_swapPenultimateRow_notOneElement() {
+    matrix m1 = createMatrixFromArray((int[]) {1, 2, 3, 4}, 2, 2);
+    matrix m2 = createMatrixFromArray((int[]) {1, 3, 2, 4}, 2, 2);
 
-    assert(isNonDescendingSorted(a, 4) == true);
-}
-
-void test_isNonDescendingSorted_true() {
-    int a[] = {4, 3, 2, 1};
-
-    assert(isNonDescendingSorted(a, 4) == false);
-}
-
-void test_isNonDescendingSorted_equalElements() {
-    int a[] = {1, 1, 1, 1};
-
-    assert(isNonDescendingSorted(a, 4) == false);
-}
-
-void test_hasAllNonDescendingRows_true() {
-    matrix m = createMatrixFromArray((int[]) {4, 3, 2, 1}, 2, 2);
-
-    assert(hasAllNonDescendingRows(m) == false);
-}
-
-void test_hasAllNonDescendingRows_false() {
-    matrix m = createMatrixFromArray((int[]) {1, 2, 3, 4}, 2, 2);
-
-    assert(hasAllNonDescendingRows(m) == true);
-}
-
-void test_countNonDescendingRowsMatrices_notZeroNonDescendingRowsMatrices() {
-    matrix *ms = createArrayOfMatrixFromArray((int[]) {1, 2, 3, 4}, 2, 2, 2);
-
-    assert(countNonDescendingRowsMatrices(ms, 2) == 2);
-}
-
-void test_countNonDescendingRowsMatrices_zeroNonDescendingRowsMatrices() {
-    matrix *ms = createArrayOfMatrixFromArray((int[]) {4, 3, 2, 1}, 2, 2, 2);
-
-    assert(countNonDescendingRowsMatrices(ms, 2) == 0);
+    assert(twoMatricesEqual(m1, m2));
 }
 
 void test() {
-    test_isNonDescendingSorted_true();
-    test_isNonDescendingSorted_false();
-    test_isNonDescendingSorted_equalElements();
-    test_hasAllNonDescendingRows_true();
-    test_hasAllNonDescendingRows_false();
-    test_countNonDescendingRowsMatrices_notZeroNonDescendingRowsMatrices();
-    test_countNonDescendingRowsMatrices_zeroNonDescendingRowsMatrices();
+    test_swapPenultimateRow_notOneElement();
+    test_swapPenultimateRow_oneElement();
 }
 
 int main() {
     test();
 
-    int nMatrices;
-    scanf("%d", &nMatrices);
     int nRows;
     scanf("%d", &nRows);
-    int nCols;
-    scanf("%d", &nCols);
 
-    matrix *ms = getMemArrayOfMatrices(nMatrices, nRows, nCols);
-    inputMatrices(ms, nMatrices);
+    matrix m = getMemMatrix(nRows, nRows);
+    inputMatrix(m);
 
-    printf("%d", countNonDescendingRowsMatrices(ms, nMatrices));
+    swapPenultimateRow(m);
+
+    outputMatrix(m);
 
     return 0;
 }

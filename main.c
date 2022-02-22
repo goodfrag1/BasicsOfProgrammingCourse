325b2698bf38968d7cc51ab1c905358e6170dfcb
goodfrag1 <GiGaBiT2017@yandex.ru>
2022-02-22 21:27:58 +0300

    2 task(tests)

--- main.c
+++ main.c
@@ -2,30 +2,29 @@
 #include "Libs/data_structures/matrix/matrix.h"
 #include "Libs/algorithms/functions/function.h"
 
-int getMin(int *a, int n) {
-    int minElement = a[0];
+int getMax(int *a, int n) {
+    int maxElement = a[0];
     for (size_t i = 1; i < n; i++)
-        if (a[i] < minElement)
-            minElement = a[i];
-
-    return minElement;
+        if (a[i] > maxElement)
+            maxElement = a[i];
+    return maxElement;
 }
 
-void sortColsByMinElement(matrix m) {
-    insertionSortColsMatrixByColCriteria(m, getMin);
+void sortRowsByMaxElement(matrix m) {
+    insertionSortRowsMatrixByRowCriteria(m, getMax);
 }
 
-void test_sortColsByMinElement() {
-    matrix m1 = createMatrixFromArray((int[]) {3, 5, 2, 4, 3, 3, 2, 5, 1, 8, 2, 7, 6, 1, 4, 4, 8, 3}, 3, 6);
-    matrix m2 = createMatrixFromArray((int[]) {5, 2, 3, 3, 3, 4, 5, 1, 2, 2, 7, 8, 1, 4, 6, 8, 3, 4}, 3, 6);
+void test_sortRowsByMaxElement() {
+    matrix m1 = createMatrixFromArray((int[]) {7, 1, 2, 1, 8, 1, 3, 2, 2}, 3, 3);
+    matrix m2 = createMatrixFromArray((int[]) {3, 2, 3, 7, 1, 2, 1, 8, 1}, 3, 3);
 
-    sortColsByMinElement(m1);
+    sortRowsByMaxElement(m1);
 
     assert(twoMatricesEqual(m1, m2));
 }
 
 void test() {
-    test_sortColsByMinElement();
+    test_sortRowsByMaxElement();
 }
 
 int main() {
@@ -39,7 +38,7 @@ int main() {
     matrix m = getMemMatrix(nRows, nCols);
     inputMatrix(m);
 
-    sortColsByMinElement(m);
+    sortRowsByMaxElement(m);
 
     outputMatrix(m);
 

31f72db8c1873354747f69aa373bcf83123b2441
goodfrag1 <GiGaBiT2017@yandex.ru>
2022-02-22 10:39:31 +0300

    18 task

--- main.c
+++ main.c
@@ -2,56 +2,29 @@
 #include "Libs/data_structures/matrix/matrix.h"
 #include "Libs/algorithms/functions/function.h"
 
-int countValues(const int *a, int n, int value) {
-    int count = 0;
-    for (int i = 0; i < n; ++i)
-        if (a[i] == value)
-            count++;
+long long getScalarProductRowAndCol(matrix m, int i, int j) {
+    long long scalarProduct = 0;
+    for (int k = 0; k < m.nRows; ++k)
+        scalarProduct += m.values[i][k] * m.values[k][j];
 
-    return count;
+    return scalarProduct;
 }
 
-int countZeroRows(matrix m) {
-    int count = 0;
-    for (int i = 0; i < m.nRows; ++i)
-        if (countValues(m.values[i], m.nCols, 0) == m.nCols)
-            count++;
+long long getSpecialScalarProduct(matrix m) {
+    int minElementCol = getMinValuePos(m).colIndex;
+    int maxElementRow = getMaxValuePos(m).rowIndex;
 
-    return count;
-}
-
-void printMatrixWithMaxZeroRows(matrix *ms, int nMatrix) {
-    int maxZeroRows = 0;
-    for (int i = 0; i < nMatrix; ++i)
-        if (countZeroRows(ms[i]) > maxZeroRows)
-            maxZeroRows = countZeroRows(ms[i]);
-
-    int k = 0;
-    int matrixWithMaxZeroRowsIndexes[nMatrix];
-    for (int i = 0; i < nMatrix; ++i)
-        if (countZeroRows(ms[i]) == maxZeroRows) {
-            matrixWithMaxZeroRowsIndexes[k] = i;
-            k++;
-        }
-
-    for (int i = 0; i < k; ++i) {
-        outputMatrix(ms[matrixWithMaxZeroRowsIndexes[i]]);
-        printf("\n");
-    }
+    return getScalarProductRowAndCol(m, maxElementRow, minElementCol);
 }
 
 int main() {
-    int nMatrices;
-    scanf("%d", &nMatrices);
     int nRows;
     scanf("%d", &nRows);
-    int nCols;
-    scanf("%d", &nCols);
 
-    matrix *ms = getMemArrayOfMatrices(nMatrices, nRows, nCols);
-    inputMatrices(ms, nMatrices);
+    matrix m = getMemMatrix(nRows, nRows);
+    inputMatrix(m);
 
-    printMatrixWithMaxZeroRows(ms, nMatrices);
+    printf("%lld", getSpecialScalarProduct(m));
 
     return 0;
 }
